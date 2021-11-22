445fed156391121ca59ead1d36b57bcf9c12778e
antonio773 <77169536+antonio773@users.noreply.github.com>
2021-11-22 12:36:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function jogos_click(){
+    document.getElementById("jogos").style.display="block";
+    document.getElementById("menu").style.display="none";
+    alert("this site is just a test, please exit");
+}
+function inicio_click(){
+    document.getElementById("menu").style.display="block";
+    document.getElementById("jogos").style.display="none";
+    alert("this site is just a test, please exit");
+}
+alert("this site is just a test, please exit");
